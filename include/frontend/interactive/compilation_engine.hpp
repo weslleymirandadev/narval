395e3c609d532c9d299ca1c27036b83265b9397e
weslleymirandadev <contato@weslleymiranda.dev>
2026-04-01 23:31:00 -0300

    fix(write): now it writes the output only once

--- include/frontend/interactive/compilation_engine.hpp
+++ include/frontend/interactive/compilation_engine.hpp
@@ -29,7 +29,7 @@ private:
     bool process_imports(const std::string& input, std::unique_ptr<Node>& ast);
     void collect_repl_names(Node* node, std::unordered_set<std::string>& defined, std::unordered_set<std::string>& used);
     bool handle_special_loop_cases(std::unique_ptr<Node>& ast, const std::unordered_set<std::string>& defined_this_line, const std::unordered_set<std::string>& used_this_line, const std::vector<std::string>& slot_names);
-    bool compile_expression(std::unique_ptr<Node>& ast, const std::unordered_set<std::string>& defined_this_line, const std::unordered_set<std::string>& used_this_line, const std::vector<std::string>& slot_names);
+    bool compile_expression(std::unique_ptr<Node>& ast, const std::unordered_set<std::string>& defined_this_line, const std::unordered_set<std::string>& used_this_line, const std::vector<std::string>& slot_names, bool single_write_call);
     bool should_print_result(const std::unordered_set<std::string>& defined_this_line, const std::unordered_set<std::string>& used_this_line, bool single_write_call, bool single_declaration_no_print, bool single_identifier_expr);
     void box_and_store_result(llvm::Value* result, llvm::Value* out_param, llvm::IRBuilder<>& builder, llvm::Module* module);
 };
