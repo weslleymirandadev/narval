1c15627a1b77b3009e106f2b50ca5b5a1ce262b3
weslleymirandadev <contato@weslleymiranda.dev>
2026-04-28 11:45:28 -0300

    feat: added multiline strings

--- src/frontend/lexer/string_tokenizer.cpp
+++ src/frontend/lexer/string_tokenizer.cpp
@@ -1,10 +1,12 @@
 #include "frontend/lexer/string_tokenizer.hpp"
 #include <stdexcept>
 
-Token tokenize_string(const std::string& input, size_t& position, size_t line, size_t column, const std::string& filename) {
+Token tokenize_string(const std::string& input, size_t& position, size_t& line, size_t& column, const std::string& filename) {
     size_t start_pos = position;
+    size_t start_line = line;
     size_t start_col = column;
-    char quote = input[position]; // ' or "
+    char quote = input[position]; // ' or " or `
+    bool is_backtick_string = quote == '`';
 
     // Advances the initial quote
     ++position;
@@ -24,6 +26,7 @@ Token tokenize_string(const std::string& input, size_t& position, size_t line, s
                 case '\\': value += '\\'; break;
                 case '\'': value += '\''; break;
                 case '"': value += '"'; break;
+                case '`': value += '`'; break;
                 default: value += '\\'; value += c; // keep unknown
             }
             escaped = false;
@@ -33,9 +36,17 @@ Token tokenize_string(const std::string& input, size_t& position, size_t line, s
             // End of string
             ++position;
             ++column;
-            return Token(TokenType::STRING, value, line, start_col, column, start_pos, position, filename);
+            return Token(TokenType::STRING, value, start_line, start_col, column, start_pos, position, filename);
         } else if (c == '\n') {
-            throw std::runtime_error("Line break not allowed inside string literal");
+            if (!is_backtick_string) {
+                throw std::runtime_error("Line break not allowed inside string literal");
+            }
+
+            value += c;
+            ++position;
+            ++line;
+            column = 1;
+            continue;
         } else {
             value += c;
         }
