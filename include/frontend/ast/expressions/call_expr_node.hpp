3fb2a84863b6180208d5d2e4621a128a872254be
weslleymirandadev <contato@weslleymiranda.dev>
2026-05-07 21:17:47 -0300

    feat(ast): change CallExprNode args from Expr to ArgNode vector

--- include/frontend/ast/expressions/call_expr_node.hpp
+++ include/frontend/ast/expressions/call_expr_node.hpp
@@ -2,22 +2,23 @@
 #include "../types.hpp"
 #include <vector>
 #include <memory>
+#include "arg_node.hpp"
 
 class CallExprNode : public Expr {
 public:
     std::unique_ptr<Expr> caller;
-    std::vector<std::unique_ptr<Expr>> args;
+    std::vector<std::unique_ptr<ArgNode>> args;
 
-    CallExprNode(std::unique_ptr<Expr> caller, std::vector<std::unique_ptr<Expr>> args)
+    CallExprNode(std::unique_ptr<Expr> caller, std::vector<std::unique_ptr<ArgNode>> args)
         : Expr(NodeType::CallExpression), caller(std::move(caller)), args(std::move(args)) {}
 
     ~CallExprNode() override = default;
 
     Node* clone() const override {
         auto cloned_caller = caller ? std::unique_ptr<Expr>(static_cast<Expr*>(caller->clone())) : nullptr;
-        std::vector<std::unique_ptr<Expr>> cloned_args;
+        std::vector<std::unique_ptr<ArgNode>> cloned_args;
         for (const auto& expr : args) {
-            cloned_args.push_back(std::unique_ptr<Expr>(static_cast<Expr*>(expr->clone())));
+            cloned_args.push_back(std::unique_ptr<ArgNode>(static_cast<ArgNode*>(expr->clone())));
         }
         auto* node = new CallExprNode(std::move(cloned_caller), std::move(cloned_args));
         if (position) {
