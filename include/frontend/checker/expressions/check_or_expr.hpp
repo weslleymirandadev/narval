d1a43d71b130e71dc2114f4b78c9910907525628
weslleymirandadev <contato@weslleymiranda.dev>
2026-09-15 00:32:06 -0300

    checker: forward declare Type as struct, matching its definition

--- include/frontend/checker/expressions/check_or_expr.hpp
+++ include/frontend/checker/expressions/check_or_expr.hpp
@@ -1,4 +1,4 @@
 #pragma once
 #include <memory>
 class Node;
-namespace nv { class Type; class Checker; std::shared_ptr<Type>& check_or_expr(Checker* checker, Node* node); }
+namespace nv { struct Type; class Checker; std::shared_ptr<Type>& check_or_expr(Checker* checker, Node* node); }
