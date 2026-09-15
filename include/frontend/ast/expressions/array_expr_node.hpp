c1bbab220a0814217d7d714040694cd985b380db
weslleymirandadev <contato@weslleymiranda.dev>
2026-09-15 00:32:07 -0300

    ast: declare ArrayExprNode's destructor out-of-line (vtable anchor)

--- include/frontend/ast/expressions/array_expr_node.hpp
+++ include/frontend/ast/expressions/array_expr_node.hpp
@@ -10,7 +10,7 @@ public:
     ArrayExprNode(std::vector<std::unique_ptr<Expr>> elements)
         : Expr(NodeType::ArrayExpression), elements(std::move(elements)) {}
 
-    ~ArrayExprNode() override = default;
+    ~ArrayExprNode() override;
 
     Node* clone() const override {
         std::vector<std::unique_ptr<Expr>> cloned_elements;
